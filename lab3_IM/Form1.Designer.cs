734706322f5399b13c52f6758cb8e4abf27d25e9
TropinDmitry <85580920+TropinDmitry@users.noreply.github.com>
2022-03-07 17:48:24 +0700

    Initial commit

---
+++ lab3_IM/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace lab3_IM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Правило";
+            // 
+            // edRule
+            // 
+            this.edRule.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edRule.Location = new System.Drawing.Point(105, 88);
+            this.edRule.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(64, 26);
+            this.edRule.TabIndex = 1;
+            this.edRule.Value = new decimal(new int[] {
+            101,
+            0,
+            0,
+            0});
+            this.edRule.ValueChanged += new System.EventHandler(this.edRule_ValueChanged);
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btStart.Location = new System.Drawing.Point(37, 306);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(97, 39);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(185, 22);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(514, 406);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // btStop
+            // 
+            this.btStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btStop.Location = new System.Drawing.Point(37, 389);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(97, 39);
+            this.btStop.TabIndex = 4;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 450);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.edRule);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btStop;
+    }
+}
+
---
+++ lab3_IM/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3_IM
+{
+    public partial class Form1 : Form
+    {
+        private int rows = 1, columns = 15;
+        private bool canEdit = true;
+        private char[] rule = new char[8];
+        private string[] Pattern = new string[] { 
+            "111", "110", "101", "100", "011", "010", "001", "000" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridView1.Columns.Add("", "");
+            }
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[0].HeaderCell.Value = rows.ToString();
+
+            setrule(Convert.ToString((int)edRule.Value, 2));
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void edRule_ValueChanged(object sender, EventArgs e)
+        {
+            setrule(Convert.ToString((int)edRule.Value, 2));
+        }
+
+        private void setrule(string props)
+        {
+            int l = props.Length;
+            while (l < 8)
+            {
+                props = "0" + props;
+                l = props.Length;
+            }
+            for (int i = 0; i < props.Length; i++)
+            {
+                rule[i] = props[i];
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            char[] oldRow = new char[columns];
+            char[] newRow = new char[columns];
+
+            for (int i = 0; i < columns; i++)
+            {
+                if (dataGridView1[i, rows - 1].Style.BackColor == Color.ForestGreen) oldRow[i] = '1';
+                else oldRow[i] = '0';
+            }
+            
+            for (int i = 0; i < columns; i++)
+            {
+                string pattern;
+                int prevNum = i - 1;
+                int nextNum = i + 1;
+                var builder = new StringBuilder();
+
+                if (i == 0) { 
+                    prevNum = columns - 1; 
+                }
+                if (i == columns - 1) { 
+                    nextNum = 0; 
+                }
+
+                builder.Append(oldRow[prevNum]);
+                builder.Append(oldRow[i]);
+                builder.Append(oldRow[nextNum]);
+
+                pattern = builder.ToString();
+
+                int index = Array.IndexOf(Pattern, pattern);
+                newRow[i] = rule[index];
+
+            }
+
+            rows++;
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[rows - 1].HeaderCell.Value = rows.ToString();
+
+            for (int i = 0; i < columns; i++)
+            {
+                if (newRow[i] == '1')  
+                    dataGridView1[i, rows-1].Style.BackColor = Color.ForestGreen;
+                else 
+                    dataGridView1[i, rows-1].Style.BackColor = Color.White;
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (canEdit)
+            {
+                switch (e.Button)
+                {
+                    case MouseButtons.Left:
+                        dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.ForestGreen;
+                        dataGridView1.ClearSelection();
+                        break;
+                    case MouseButtons.Right:
+                        dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.White;
+                        dataGridView1.ClearSelection();
+                        break;
+                }
+            }
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            canEdit = false;
+            timer1.Stop();
+        }
+    }
+}
